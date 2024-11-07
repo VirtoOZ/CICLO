5e7b14bc1cfbc65d2fb6c5737dcb4be705be5d37
VirtoOZ <109903427+VirtoOZ@users.noreply.github.com>
2024-11-07 21:15:57 +0300

    Проверка отключение loadedclass

--- js/app.js
+++ js/app.js
@@ -34,13 +34,6 @@
             return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
         }
     };
-    function addLoadedClass() {
-        window.addEventListener("load", (function() {
-            setTimeout((function() {
-                document.documentElement.classList.add("loaded");
-            }), 0);
-        }));
-    }
     let _slideUp = (target, duration = 500, showmore = 0) => {
         if (!target.classList.contains("_slide")) {
             target.classList.add("_slide");
@@ -774,7 +767,6 @@
         }
     }), 0);
     isWebp();
-    addLoadedClass();
     menuInit();
     spollers();
     digitsCounter();
